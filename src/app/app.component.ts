200f066900129269272d9790977e4a0ec54612a6
CaruBardd <diego35andres199@gmail.com>
2022-04-19 21:34:28 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name: string = 'Diego';
+  age: number = 22;
+  img: string = 'https://source.unsplash.com/random';
 }
