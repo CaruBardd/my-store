9a25539ed64d83dc2755b7f220e86673790981ac
CaruBardd <diego35andres199@gmail.com>
2022-04-19 21:52:55 -0500

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name: string = 'Diego';
   age: number = 22;
   img: string = 'https://source.unsplash.com/random';
+  btnDisabled: boolean = true;
+  person = {
+    name: 'Diego',
+    age: 22,
+    avatar: 'https://source.unsplash.com/random'
+  }
 }
